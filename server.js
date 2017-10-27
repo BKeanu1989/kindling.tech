7ebf61fc46e1a6b9d9958d669d2842dc4d5d0ae0
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-10-27 14:48:20 +0200

    display index.html via express.static

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-  res.send('via git working??');
+  res.render('index');
 });
 
 
